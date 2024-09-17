312c23856e68dc481f8d5176837e92ddde31240d
Mathis FLAVIN <mathis.flavin@gmail.com>
2024-09-17 18:45:05 +0200

    delay

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ import puppeteer from 'puppeteer';
 // Or import puppeteer from 'puppeteer-core';
 
 // Launch the browser and open a new blank page
-const browser = await puppeteer.launch();
+const browser = await puppeteer.launch({headless: false});
 const page = await browser.newPage();
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 // Navigate the page to a URL.
 await page.goto('https://univ.scholarvox.com/reader/docid/88814086/page/1');
+
+console.info("avant")
+await delay(20000);
+console.info("après")
 
 await page.screenshot({
     path: 'hn.png',
   });
 
 // Set screen size.
-await page.setViewport({width: 910, height: 1176});
+await page.setViewport({width: 1000, height: 913});
 
 await browser.close();
